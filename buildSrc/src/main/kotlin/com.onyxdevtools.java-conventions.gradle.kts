56bdaf8fff2ea0f108e0e21598e82371851325f2
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 <timbob1000@gmail.com>
2025-04-11 07:09:08 -0600

    Tweak normalization

--- buildSrc/src/main/kotlin/com.onyxdevtools.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.onyxdevtools.java-conventions.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 }
 
 group = "com.onyxdevtools"
-version = "3.5.16"
+version = "3.5.17"
 java.sourceCompatibility = JavaVersion.VERSION_17
 java.targetCompatibility = JavaVersion.VERSION_17
 
--- onyx-database-examples/indexes/build.gradle.kts
+++ onyx-database-examples/indexes/build.gradle.kts
@@ -20,11 +20,11 @@ repositories {
 }
 
 dependencies {
-    implementation("com.onyxdevtools:onyx-database:3.5.16")
+    implementation("com.onyxdevtools:onyx-database:3.5.17")
 }
 
 group = "com.onyxdevtools"
-version = "3.5.16"
+version = "3.5.17"
 description = "com.onyxdevtools:onyx-database-examples:indexes"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
