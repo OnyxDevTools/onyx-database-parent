528d591182bd7e6e43289ad0262064376b2de738
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 <timbob1000@gmail.com>
2025-09-12 06:59:59 -0600

    update version number

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Config {
     // Onyx Version
-    const val ONYX_VERSION = "3.5.3"
+    const val ONYX_VERSION = "3.6.0"
 
     // Took Versions
     const val JAVA_VERSION = 21
