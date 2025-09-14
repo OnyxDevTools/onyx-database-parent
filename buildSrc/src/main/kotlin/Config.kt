356d88fe29190dad2d5cf60322e225966e203dc2
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 <timbob1000@gmail.com>
2025-09-13 19:17:22 -0600

    fix build

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -8,6 +8,7 @@ object Config {
     const val JAVA_VERSION = 21
     val JAVA_TARGET = JavaVersion.VERSION_21
     const val KOTLIN_VERSION = "2.2.10"
+    const val GSON_VERSION = "2.13.1"
 
     // 3rd Party Dependencies ( HTTP Client )
     const val KTOR_VERSION = "3.2.3"
--- onyx-cloud-client/build.gradle.kts
+++ onyx-cloud-client/build.gradle.kts
@@ -6,6 +6,12 @@ plugins {
     id("dev.onyx.java-conventions")
 }
 
+dependencies {
+    implementation("com.google.code.gson:gson:${Config.GSON_VERSION}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+}
+
 java {
     withJavadocJar()
 }
