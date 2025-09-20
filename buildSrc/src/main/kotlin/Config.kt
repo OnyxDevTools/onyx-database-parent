ff0e41c6a07cb03a865530e36a1753df001fd9d7
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 <timbob1000@gmail.com>
2025-09-19 19:54:47 -0600

    Fix change when deleting an entity, you delete both the data and index files

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Config {
     // Onyx Version
-    const val ONYX_VERSION = "3.6.6"
+    const val ONYX_VERSION = "3.6.7"
 
     // Took Versions
     const val JAVA_VERSION = 21
--- onyx-database/src/main/kotlin/com/onyx/lang/concurrent/impl/DefaultClosureReadWriteLock.kt
+++ onyx-database/src/main/kotlin/com/onyx/lang/concurrent/impl/DefaultClosureReadWriteLock.kt
@@ -1,6 +1,5 @@
 package com.onyx.lang.concurrent.impl
 
-import com.onyx.interactors.classfinder.ApplicationClassFinder
 import com.onyx.lang.concurrent.ClosureReadWriteLock
 
 class DefaultClosureReadWriteLock : ClosureReadWriteLock {
@@ -8,24 +7,11 @@ class DefaultClosureReadWriteLock : ClosureReadWriteLock {
     private val lockImplementation:ClosureReadWriteLock = createLock()
 
     override fun <T> optimisticReadLock(consumer: () -> T): T = lockImplementation.readLock(consumer)
-    override fun <T> readLock(consumer: () -> T): T = lockImplementation.readLock(consumer)
+    override fun <T> readLock(consumer: () -> T): T = lockImplementation.optimisticReadLock(consumer)
     override fun <T> writeLock(consumer: () -> T): T = lockImplementation.writeLock(consumer)
 
     companion object {
 
-        fun createLock() = if(lockClass === LockType.STAMP_LOCK) StampedClosureReadWriteLock() else ClosureReadWriteCompatLock()
-
-        private val lockClass by lazy {
-            try {
-                ApplicationClassFinder.forName("java.util.concurrent.locks.StampedLock")
-                LockType.STAMP_LOCK
-            } catch (e:Exception){
-                LockType.REENTRANT_READ_WRITE
-            }
-        }
-
-        enum class LockType{
-            STAMP_LOCK, REENTRANT_READ_WRITE
-        }
+        fun createLock() = ClosureReadWriteCompatLock()
     }
 }
