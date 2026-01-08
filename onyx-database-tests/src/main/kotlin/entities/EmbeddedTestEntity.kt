90ffc75fce859d4fd754f489185dc3a3c437d226
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 <timbob1000@gmail.com>
2026-01-08 09:55:14 -0700

    Fix issue with querying list resolvers

--- onyx-database-tests/src/main/kotlin/entities/EmbeddedTestEntity.kt
+++ onyx-database-tests/src/main/kotlin/entities/EmbeddedTestEntity.kt
@@ -12,4 +12,22 @@ data class EmbeddedTestEntity(
     var id: Long = 0,
     @Attribute
     var embeddedAttribute: Map<String, Any?> = emptyMap()
+) : ManagedEntity() {
+    val child: Any?
+        get() = ResolverChild(
+            name = "test"
+        )
+
+    val children: Any?
+        get() = arrayListOf(ResolverChild(
+            name = "test"
+        ))
+}
+
+@Entity
+class ResolverChild(
+    @Identifier(generator = IdentifierGenerator.SEQUENCE)
+    var id: Long = 0,
+    @Attribute
+    var name: String? = null
 ) : ManagedEntity()
