f26c329f965d0520fc571db13b659930f7ef5e91
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 <timbob1000@gmail.com>
2026-01-08 22:07:12 -0700

    bump onyx version

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Config {
     // Onyx Version
-    const val ONYX_VERSION = "3.7.1"
+    const val ONYX_VERSION = "3.7.2"
 
     // Took Versions
     const val JAVA_VERSION = 21
