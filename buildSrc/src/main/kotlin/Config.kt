fc7fa89f05f7fd5a19ebea7dfe7c7cfd1fb7e4fc
tosborn <tosborn@onyx.dev>
2026-01-09 20:04:18 -0700

    Remove old dependency

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Config {
     // Onyx Version
-    const val ONYX_VERSION = "3.7.2"
+    const val ONYX_VERSION = "3.7.3"
 
     // Took Versions
     const val JAVA_VERSION = 21
--- onyx-lucene-index/build.gradle.kts
+++ onyx-lucene-index/build.gradle.kts
@@ -13,7 +13,6 @@ dependencies {
     implementation(project(":onyx-database"))
 
     implementation("org.apache.lucene:lucene-core:${Config.LUCENE_VERSION}")
-    implementation("org.apache.lucene:lucene-analyzers-common:8.11.4")
     implementation("org.apache.lucene:lucene-queryparser:${Config.LUCENE_VERSION}")
 }
 
