860828d93387e92efe89c5ba051a5058dde7346e
tosborn <tosborn@onyx.dev>
2026-09-03 13:23:57 -0600

    Version bump

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Config {
     // Onyx Version
-    const val ONYX_VERSION = "4.2.0"
+    const val ONYX_VERSION = "4.2.1"
 
     // Took Versions
     const val JAVA_VERSION = 23
